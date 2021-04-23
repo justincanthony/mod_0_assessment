045b938876af5dd60e9e8b1db144c73fb8ce4330
Justin Anthony <justincanthony@yahoo.com>
2021-04-23 11:01:22 -0600

    add object to file

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var airportItaly = {
+  name: "Malpensa",
+  location: ("Milan"),
+  numOfTerminals: 4,
+  numOfairlines: 20,
+  isConvenient: true,
+  isInternational: true,
+  accommodations: ["hotels", "food", "transportation"],
+  transporationServices: ["taxi", "bus", "private"]
+};
