e744351eaf7c5341a64ed257f4618fb3ed81fbbe
Justin Anthony <justincanthony@yahoo.com>
2021-04-23 11:07:49 -0600

    Add object to file

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var newCar = {
+  manufacturer: "Tesla",
+  model: "Cybertruck",
+  productionYear: 2022,
+  availableYear: 2023,
+  isCool: true,
+  canAfford: false,
+  interiorFeatures: ["leather", "custom stiching", "colorway"],
+  mechanichalFeatures: ["awd", "self-drive"]
+};
